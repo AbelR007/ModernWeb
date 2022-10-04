b4f3ffc706e874a4e9b78cac80d8b076beb53413
AbelR007 <abelroi007@gmail.com>
2022-10-04 20:14:10 +0530

    v1.3.1 - Published
    Site is now live in Github Pages

--- src/App.js
+++ src/App.js
@@ -6,20 +6,39 @@ import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import React, { useEffect} from "react";
+// import DocumentMeta from 'react-document-meta';
 
 function App() {
+  // useEffect(() => {
+  //   document.title = "Abel Roy.";
+  //   document.description = "Portfolio Website developed on React JS";
+  //   document.getElementsByTagName("META")[2].content="Portfolio Website developed on React";
+  //   }, []);
+  // const meta = {
+  //   title: 'Abel Roy',
+  //   description: 'Portfolio Website developed on React JS',
+  //   meta: {
+  //       charset: 'utf-8',
+  //       name: {
+  //           keywords: 'react,javascript,portfolio,abelroy,website,gsap'
+  //       }
+  //   }
+  // }
+
   return (
     <>
-    <Routes>
-      <Route path='/' element={<Layout/>}>
-        <Route index element={<Home/>}/>
-        <Route path='about' element={<About/>}/>
-        <Route path='projects' element={<Projects/>}/>
-        <Route path='contact' element={<Contact/>}/>
-      </Route>
-    </Routes>
+      {/* <DocumentMeta {...meta} /> */}
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+      </Routes>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
